056b7e3413e2e298c29df596868850969349c8ce
Jixiang SUN <snownamida@outlook.com>
2025-12-28 17:12:40 +0100

    lyon-server

---
+++ lyon-server/src/main/java/com/github/snownamida/lyon_server/LyonServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.snownamida.lyon_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyonServerApplication.class, args);
+	}
+
+}
---
+++ lyon-server/src/test/java/com/github/snownamida/lyon_server/LyonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.snownamida.lyon_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
