e6290f864eae53903de7f6aa58ec4d9d3caf5eff
Jixiang SUN <snownamida@outlook.com>
2025-12-31 16:40:14 +0100

    继续尝试修复

---
+++ lyon-server/src/main/java/com/github/snownamida/lyon_server/controller/TransportLineController.java
@@ -0,0 +1,24 @@
+package com.github.snownamida.lyon_server.controller;
+
+import com.github.snownamida.lyon_server.service.TransportLineService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lines")
+public class TransportLineController {
+
+    private final TransportLineService transportLineService;
+
+    public TransportLineController(TransportLineService transportLineService) {
+        this.transportLineService = transportLineService;
+    }
+
+    @GetMapping(value = "/{type}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getLines(@PathVariable String type) {
+        return transportLineService.getCachedLineData(type);
+    }
+}
